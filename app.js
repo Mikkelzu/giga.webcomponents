2023d6ff6d1c379f7212b7d4c3a37f8607205882
Michael <michael@mbp-van-michael.home>
2020-08-25 09:43:03 +0200

    created branch for release candidates

--- app.js
+++
@@ -1,8 +0,0 @@
-
-function toast(input) {
-    Toast('success', {text: input, icon: 'far fa-check-circle fa-lg'});
-}
-
-function modal() {
-    giga.modal({title: 'Modal title', buttons: {cancel: 'Cancel', ok: 'Ok'}});
-}
